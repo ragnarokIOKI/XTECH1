bcc11b3ae98e48a8d0941f5ae1b3b9d9fe4973e0
Ioki Onyx <isip_v.g.lavrinova@mpt.ru>
2024-11-13 17:26:23 +0300

    Вывод списка сетей в DataGrid

--- XTECHLavrinova1/MainWindow.xaml.cs
+++ XTECHLavrinova1/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,32 +14,48 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SimpleWifi.Win32;
+using SimpleWifi.Win32.Interop;
 
 namespace XTECHLavrinova1
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private string connectionString = "Data Source=DESKTOP-8J6N5LS\\SQLEXPRESS;Initial Catalog=XTECTWifi_Database;Persist Security Info=True;User ID=sa;Password=72154;Encrypt=True;TrustServerCertificate=True";
+        private static WlanClient _client = new WlanClient();
+        private WifiScanner wifiScanner = new WifiScanner(_client);
+        private List<WifiSSID_Model> networks = new List<WifiSSID_Model>();
+
         public MainWindow()
         {
             InitializeComponent();
+            LoadData();
         }
 
-        private void dtWiFi_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void LoadData()
         {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var query = @" select
+	                        [Name_SSID] as 'Имя сети',
+	                        [Wifi_Status] as 'Уровень сигнала сети',
+	                        [Date_Add] as 'Дата сохранения сети'
+                        from Wifi_SSID";
 
-        }
-
-        private void dtWiFi_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
-        {
-
+                using (var command = new SqlCommand(query, connection))
+                {
+                    var dataTable = new DataTable();
+                    new SqlDataAdapter(command).Fill(dataTable);
+                    dtWiFi.ItemsSource = dataTable.DefaultView;
+                }
+            }
         }
 
         private void btnScan_Click(object sender, RoutedEventArgs e)
         {
-
+            List<WifiSSID_Model> networks = wifiScanner.GetAvailableWifiSSID_Models();
+            dtWiFi.ItemsSource = networks;
         }
 
         private void btnSave_Click(object sender, RoutedEventArgs e)
@@ -47,7 +65,11 @@ namespace XTECHLavrinova1
 
         private void btnExit_Click(object sender, RoutedEventArgs e)
         {
-
+            switch (MessageBox.Show("Вы уверены, что хотите выйти?", "Выход из приложения", MessageBoxButton.YesNo)){
+                case MessageBoxResult.Yes:
+                    Close();
+                    break;
+            };
         }
     }
 }
---
+++ XTECHLavrinova1/WifiSSID_Model.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XTECHLavrinova1
+{
+    public class WifiSSID_Model
+    {
+        public string Name_SSID { get; set; }
+        public int Wifi_Status { get; set; }
+        public DateTime Date_Add { get; set; }
+    }
+}
